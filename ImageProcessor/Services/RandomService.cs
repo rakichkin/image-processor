ec20da717643b05d49abccf15934136502f3d758
Igor Rakitskii <rakichkin@gmail.com>
2022-09-03 17:42:47 +0300

    Revert "mend"
    This reverts commit b6cf346d0c315b29115d1301877de0d676ce8524.

---
+++ ImageProcessor/Services/RandomService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imageProcessor.services
+{
+	public static class RandomService
+	{
+		public static int GetRandomValue(int min, int max)
+		{
+			var rand = new Random();
+			return rand.Next(min, max);
+		}
+	}
+}
